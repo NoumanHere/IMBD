25fef90fb9bde6a614a06eb869c5644c58cc4159
NoumanHere <noumannow@gmail.com>
2020-04-30 13:48:49 +0500

    Modified Pipelines

--- Imdb/pipelines.py
+++ Imdb/pipelines.py
@@ -4,8 +4,19 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import pymongo
 
+class MongodbPipeline(object):
 
-class ImdbPipeline(object):
+    collection_name = "best_movies"
+
+    def open_spider(self,spider):
+        self.client = pymongo.MongoClient("mongodb+srv://Nouman:GDfieBq8Mjxe4QAk@cluster0-fpdvh.mongodb.net/test?retryWrites=true&w=majority")
+        self.db = self.client["IMDB"]
+
+    def close_spider(self,spider):
+        self.client.close()
+
     def process_item(self, item, spider):
+        self.db[self.collection_name].insert(item)
         return item
